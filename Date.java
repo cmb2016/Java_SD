63322c575616c7c163b12a84a38d97760227f170
cmb2016 <clairemariebacani@gmail.com>
2016-11-28 14:51:14 +0000

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,54 +1,76 @@
-/**
- * Created by CMB on 02/11/2016.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
+/**
+ *
+ * @author C13555907
+ */
 class Date implements Comparable <Date>{
-
-    private int day;
-    private int month;
+    
+    //Public data members declared here
+    private int day; 
+    private int month; 
     private int year;
-
+    
     //User-defined constructor
     public Date(int d, int m, int y) {
-        setDay(d);
-        setMonth(m);
-        setYear(y);
+       try {
+           setDay(d);
+       }catch(IllegalArgumentException e) {
+           System.out.println("Exception thrown  :" + e);
+       }
+       try {
+           setMonth(m);
+       }catch(IllegalArgumentException e) {
+            System.out.println("Exception thrown  :" + e);
+       }
+       try {
+           setYear(y);
+       }catch(IllegalArgumentException e) {
+           System.out.println("Exception thrown  :" + e);
+       }
     }
-
-    //Setters & Getters
+    
+    //public member methods
     public void setDay(int d){
-        if(d > 0 || d < 32){
+        if(d > 0 && d < 32){
             day = d;
         }else{
-            System.out.println("Invalid day!");
+            throw new IllegalArgumentException("Invalid day! ");
         }
     }
-
     public void setMonth(int m){
-        if(m > 0 || m < 13){
+        if(m > 0 && m < 13){
             month = m;
         }else{
-            System.out.println("Invalid month!");
+            throw new IllegalArgumentException("Invalid month! ");
         }
     }
-
     public void setYear(int y){
-        year = y;
+        if(y > 2015) {
+            year = y;
+        }else{
+            throw new IllegalArgumentException("Invalid year! ");
+        }
     }
-
+    
     public int getDay(){
         return day;
     }
-
     public int getMonth(){
         return month;
     }
-
     public int getYear(){
         return year;
     }
 
-    //User-defined compareTo method
+    public String toString(){
+        return this.day + "/" + this.month + "/" + this.year;
+    }
+
     @Override
     public int compareTo(Date compareDate) {
         if(compareDate.getYear() != this.getYear()){
@@ -61,14 +83,5 @@ class Date implements Comparable <Date>{
             return compareDate.getDay() - this.getDay();
         }
         return 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Date{" +
-                "day=" + day +
-                ", month=" + month +
-                ", year=" + year +
-                '}';
     }
 }
